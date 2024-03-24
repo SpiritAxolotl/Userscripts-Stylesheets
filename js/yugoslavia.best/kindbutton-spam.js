8aa302b88c62ed02d0a4b9917dfd8238897b06f7
Spax <gamecrusher218@gmail.com>
2024-03-23 18:34:37 -0600

    kind button spam update

--- js/yugoslavia.best/kindbutton-spam.js
+++ js/yugoslavia.best/kindbutton-spam.js
@@ -1,6 +1,10 @@
+//it's better to just paste this in the console than use it as a userscript tbh
+
 const timesToClick = 1000;
-for (let i=0; i<timesToClick; i++) {
+
+for (let i=0; i<timesToClick; i++)
   document.querySelector("button[onclick='javascript:swapElements()']").click();
-  //alternatively:
-  //swapElements();
-}
+
+//alternatively:
+for (let i=0; i<timesToClick; i++)
+  swapElements();
